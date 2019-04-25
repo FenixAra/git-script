f6a5df23b5cb836a33460aa94f4466b0900b517f
Aravindhan <aravindhan.a@lynk.co.in>
2019-04-26 00:33:51 +0530

    ✨ Added script to pull all latest in current branch

---
+++ pull-all-current/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	filepath.Walk(".",
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+
+			if info.Name() == ".git" {
+				path := strings.Replace(dir+"/"+path, info.Name(), "", 1)
+
+				_, err = exec.Command("git", "-C", path, "pull").
+					Output()
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				fmt.Println("Successfully updated repo: ", path)
+			}
+			return nil
+		})
+}
